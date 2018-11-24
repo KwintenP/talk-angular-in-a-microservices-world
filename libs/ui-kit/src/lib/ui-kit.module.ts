43b323795b91af9cad4cf166368bac6a90282ee6
Kwinten Pisman <kwinten.pisman@gmail.com>
2018-11-24 13:44:31 +0100

    updated the ui-kit module

--- libs/ui-kit/src/lib/ui-kit.module.ts
+++ libs/ui-kit/src/lib/ui-kit.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { TableComponent } from './table/table.component';
 
 export const uiKitRoutes: Route[] = [];
 
 @NgModule({
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule],
+  declarations: [TableComponent]
 })
 export class UiKitModule {}

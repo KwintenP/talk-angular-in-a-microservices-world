369a4109ad04bb4f1fcd799d3c106541f772ceb2
Kwinten Pisman <kwinten.pisman@gmail.com>
2018-11-24 13:13:11 +0100

    linked apps with libs

--- apps/stock-mgmt/src/app/app.module.ts
+++ apps/stock-mgmt/src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
+import { StockModule } from '@demo/stock';
+import { UiKitModule } from '@demo/ui-kit';
+import { AuthModule } from '@demo/auth';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    UiKitModule,
+    StockModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- apps/webshop/src/app/app.module.ts
+++ apps/webshop/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
+import { UiKitModule } from '@demo/ui-kit';
+import { ProductModule } from '@demo/product';
+import { PaymentModule } from '@demo/payment';
+import { AuthModule } from '@demo/auth';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    UiKitModule,
+    ProductModule,
+    PaymentModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
